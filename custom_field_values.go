9c50592a91140f5a876e6b98a7e0eabbad7e5091
florian kinder <florian.kinder@mcl.de>
2019-09-06 11:44:40 +0200

    create field values

--- custom_field_values.go
+++ custom_field_values.go
@@ -1,8 +1,10 @@
 package activecampaign
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"net/http"
 )
 
@@ -38,4 +40,32 @@ func (a *ActiveCampaign) fieldValues(ctx context.Context, pof *POF, url string) 
 
 func (a *ActiveCampaign) FieldValues(ctx context.Context, pof *POF) (*FieldValues, error) {
 	return a.fieldValues(ctx, pof, "fieldValues")
+}
+
+type CreateFieldValue struct {
+	Contact string `json:"contact"`
+	Field   string `json:"field"`
+	Value   string `json:"value"`
+}
+
+func (a *ActiveCampaign) CreateFieldValue(ctx context.Context, create CreateFieldValue) error {
+	b := new(bytes.Buffer)
+	err := json.NewEncoder(b).Encode(struct {
+		FieldValue CreateFieldValue `json:"fieldValue"`
+	}{
+		FieldValue: create,
+	})
+	if err != nil {
+		return err
+	}
+
+	res, err := a.send(ctx, http.MethodPost, "fieldValues", nil, b)
+	if err != nil {
+		return err
+	}
+	if res.StatusCode != http.StatusCreated {
+		return errors.New(res.Status)
+	}
+
+	return nil
 }
