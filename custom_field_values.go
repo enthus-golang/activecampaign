54a9caf3049eab20c3a2f3d40754ff867dc11490
florian kinder <florian.kinder@mcl.de>
2019-09-06 13:25:12 +0200

    Added update field value

--- custom_field_values.go
+++ custom_field_values.go
@@ -21,6 +21,7 @@ type FieldValue struct {
 	Value      string `json:"value"`
 	CreateDate string `json:"cdate"`
 	UpdateDate string `json:"udate"`
+	ID         string `json:"id"`
 }
 
 func (a *ActiveCampaign) fieldValues(ctx context.Context, pof *POF, url string) (*FieldValues, error) {
@@ -42,16 +43,16 @@ func (a *ActiveCampaign) FieldValues(ctx context.Context, pof *POF) (*FieldValue
 	return a.fieldValues(ctx, pof, "fieldValues")
 }
 
-type CreateFieldValue struct {
+type ChangeFieldValue struct {
 	Contact string `json:"contact"`
 	Field   string `json:"field"`
 	Value   string `json:"value"`
 }
 
-func (a *ActiveCampaign) CreateFieldValue(ctx context.Context, create CreateFieldValue) error {
+func (a *ActiveCampaign) CreateFieldValue(ctx context.Context, create ChangeFieldValue) error {
 	b := new(bytes.Buffer)
 	err := json.NewEncoder(b).Encode(struct {
-		FieldValue CreateFieldValue `json:"fieldValue"`
+		FieldValue ChangeFieldValue `json:"fieldValue"`
 	}{
 		FieldValue: create,
 	})
@@ -60,6 +61,28 @@ func (a *ActiveCampaign) CreateFieldValue(ctx context.Context, create CreateFiel
 	}
 
 	res, err := a.send(ctx, http.MethodPost, "fieldValues", nil, b)
+	if err != nil {
+		return err
+	}
+	if res.StatusCode != http.StatusCreated {
+		return errors.New(res.Status)
+	}
+
+	return nil
+}
+
+func (a *ActiveCampaign) UpdateFieldValue(ctx context.Context, id string, update ChangeFieldValue) error {
+	b := new(bytes.Buffer)
+	err := json.NewEncoder(b).Encode(struct {
+		FieldValue ChangeFieldValue `json:"fieldValue"`
+	}{
+		FieldValue: update,
+	})
+	if err != nil {
+		return err
+	}
+
+	res, err := a.send(ctx, http.MethodPut, "fieldValues/"+id, nil, b)
 	if err != nil {
 		return err
 	}
